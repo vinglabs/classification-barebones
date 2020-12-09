09fd975f2e9005e86fd67df0ba34eee1cd850b92
jasdeep06 <jasdeepchhabra94@gmail.com>
2020-12-09 08:33:36 +0530

    removed rounding of f1

--- valid.py
+++ valid.py
@@ -10,11 +10,11 @@ def calculate_class_wise_precision_recall_f1(test_predictions,test_labels,classe
         stat_dict[class_name]['tp'] = test_predictions[(test_labels == i).nonzero()].eq(i).nonzero().size(0)
         stat_dict[class_name]['fp'] = test_predictions[(test_labels != i).nonzero()].eq(i).nonzero().size(0)
         stat_dict[class_name]['fn'] = test_predictions[(test_labels == i).nonzero()].ne(i).nonzero().size(0)
-        stat_dict[class_name]['precision'] = round(stat_dict[class_name]['tp']/(stat_dict[class_name]['tp'] + stat_dict[class_name]['fp']+0.0001),2)
-        stat_dict[class_name]['recall'] = round(stat_dict[class_name]['tp']/(stat_dict[class_name]['tp'] + stat_dict[class_name]['fn']+ 0.0001),2)
-        stat_dict[class_name]['f1'] = round(2 * stat_dict[class_name]['precision'] * stat_dict[class_name]['recall']/(stat_dict[class_name]['precision'] + stat_dict[class_name]['recall']+0.0001),2)
+        stat_dict[class_name]['precision'] = stat_dict[class_name]['tp']/(stat_dict[class_name]['tp'] + stat_dict[class_name]['fp']+0.0001)
+        stat_dict[class_name]['recall'] = stat_dict[class_name]['tp']/(stat_dict[class_name]['tp'] + stat_dict[class_name]['fn']+ 0.0001)
+        stat_dict[class_name]['f1'] = 2 * stat_dict[class_name]['precision'] * stat_dict[class_name]['recall']/(stat_dict[class_name]['precision'] + stat_dict[class_name]['recall']+0.0001)
 
-    stat_dict['accuracy'] = round(test_labels.eq(test_predictions).nonzero().size(0)/test_labels.size(0),2)
+    stat_dict['accuracy'] = test_labels.eq(test_predictions).nonzero().size(0)/test_labels.size(0)
 
     return stat_dict
 
