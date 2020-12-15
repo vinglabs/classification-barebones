52f5dcb94cc93b6aa27234ea41b5f4a2dc8a9f5d
jasdeep06 <jasdeepchhabra94@gmail.com>
2020-12-15 07:09:50 +0530

    added activation to colornet

--- colornet.py
+++ colornet.py
@@ -9,10 +9,12 @@ class ColorNet(torch.nn.Module):
                                      out_channels=48,
                                      kernel_size=11,
                                      stride=4)
+        self.activation1_b1_1 = torch.nn.ReLU()
         self.conv1_b2_1 = torch.nn.Conv2d(in_channels=3,
                                         out_channels=48,
                                         kernel_size=11,
                                         stride=4)
+        self.activation1_b2_1 = torch.nn.ReLU()
         self.maxpool1_b1_1 = torch.nn.MaxPool2d(stride=2,kernel_size=3)
         self.maxpool1_b2_1 = torch.nn.MaxPool2d(stride=2,kernel_size=3)
         self.batchnorm1_b1_1 = torch.nn.BatchNorm2d(num_features=48, momentum=0.03, eps=1E-4)
@@ -27,18 +29,26 @@ class ColorNet(torch.nn.Module):
                                      out_channels=64,
                                      kernel_size=3,
                                      stride=1)
+        self.activation2_b1_1 = torch.nn.ReLU()
+
         self.conv2_b1_2 = torch.nn.Conv2d(in_channels=48,
                                           out_channels=64,
                                           kernel_size=3,
                                           stride=1)
+        self.activation2_b1_2 = torch.nn.ReLU()
+
         self.conv2_b2_1 = torch.nn.Conv2d(in_channels=48,
                                           out_channels=64,
                                           kernel_size=3,
                                           stride=1)
+        self.activation2_b2_1 = torch.nn.ReLU()
+
         self.conv2_b2_2 = torch.nn.Conv2d(in_channels=48,
                                           out_channels=64,
                                           kernel_size=3,
                                           stride=1)
+        self.activation2_b2_2 = torch.nn.ReLU()
+
         self.maxpool2_b1_1 = torch.nn.MaxPool2d(stride=2, kernel_size=3)
         self.maxpool2_b1_2 = torch.nn.MaxPool2d(stride=2, kernel_size=3)
         self.maxpool2_b2_1 = torch.nn.MaxPool2d(stride=2, kernel_size=3)
@@ -54,36 +64,48 @@ class ColorNet(torch.nn.Module):
                                      out_channels=96,
                                      kernel_size=3,
                                      stride=1,padding=1)
+        self.activation3_b1_1 = torch.nn.ReLU()
+
         self.conv3_b1_2 = torch.nn.Conv2d(in_channels=64,
                                           out_channels=96,
                                           kernel_size=3,
                                           stride=1, padding=1)
+        self.activation3_b1_2 = torch.nn.ReLU()
         self.conv3_b2_1 = torch.nn.Conv2d(in_channels=64,
                                           out_channels=96,
                                           kernel_size=3,
                                           stride=1, padding=1)
+        self.activation3_b2_1 = torch.nn.ReLU()
+
         self.conv3_b2_2 = torch.nn.Conv2d(in_channels=64,
                                           out_channels=96,
                                           kernel_size=3,
                                           stride=1, padding=1)
-
+        self.activation3_b2_2 = torch.nn.ReLU()
 
         self.conv4_b1_1 = torch.nn.Conv2d(in_channels=192,
                                      out_channels=96,
                                      kernel_size=3,
                                      stride=1,padding=1)
+        self.activation4_b1_1 = torch.nn.ReLU()
         self.conv4_b1_2 = torch.nn.Conv2d(in_channels=192,
                                      out_channels=96,
                                      kernel_size=3,
                                      stride=1, padding=1)
+        self.activation4_b1_2 = torch.nn.ReLU()
+
         self.conv4_b2_1 = torch.nn.Conv2d(in_channels=192,
                                      out_channels=96,
                                      kernel_size=3,
                                      stride=1, padding=1)
+        self.activation4_b2_1 = torch.nn.ReLU()
+
         self.conv4_b2_2 = torch.nn.Conv2d(in_channels=192,
                                      out_channels=96,
                                      kernel_size=3,
                                      stride=1, padding=1)
+        self.activation4_b2_2 = torch.nn.ReLU()
+
 
 
 
@@ -91,18 +113,25 @@ class ColorNet(torch.nn.Module):
                                      out_channels=64,
                                      kernel_size=3,
                                      stride=1)
+        self.activation5_b1_1 = torch.nn.ReLU()
         self.conv5_b1_2 = torch.nn.Conv2d(in_channels=96,
                                      out_channels=64,
                                      kernel_size=3,
                                      stride=1)
+        self.activation5_b1_2 = torch.nn.ReLU()
+
         self.conv5_b2_1 = torch.nn.Conv2d(in_channels=96,
                                      out_channels=64,
                                      kernel_size=3,
                                      stride=1)
+        self.activation5_b2_1 = torch.nn.ReLU()
+
         self.conv5_b2_2 = torch.nn.Conv2d(in_channels=96,
                                      out_channels=64,
                                      kernel_size=3,
                                      stride=1)
+        self.activation5_b2_2 = torch.nn.ReLU()
+
         self.maxpool5_b1_1 = torch.nn.MaxPool2d(stride=2, kernel_size=3)
         self.maxpool5_b1_2 = torch.nn.MaxPool2d(stride=2, kernel_size=3)
         self.maxpool5_b2_1 = torch.nn.MaxPool2d(stride=2, kernel_size=3)
@@ -111,35 +140,43 @@ class ColorNet(torch.nn.Module):
 
         if not lite:
             self.fc1 = torch.nn.Linear(in_features=4096,out_features=4096)
+            self.activation_fc1 = torch.nn.ReLU()
 
             self.dropout1 = torch.nn.Dropout(p=0.2)
 
             self.fc2 = torch.nn.Linear(in_features=4096,out_features=4096)
+            self.activation_fc2 = torch.nn.ReLU()
 
             self.dropout2 = torch.nn.Dropout(p=0.2)
 
             self.fc3 = torch.nn.Linear(in_features=4096,out_features=num_classes)
         else:
             self.fc1 = torch.nn.Linear(in_features=4096, out_features=512)
+            self.activation_fc1 = torch.nn.ReLU()
 
             self.dropout1 = torch.nn.Dropout(p=0.2)
 
             self.fc2 = torch.nn.Linear(in_features=512, out_features=num_classes)
 
 
-
     def forward(self,x):
         x1_b1_1 = self.conv1_b1_1(x)
+        x1_b1_1 = self.activation1_b1_1(x1_b1_1)
         x1_b2_1 = self.conv1_b2_1(x)
+        x1_b2_1 = self.activation1_b2_1(x1_b2_1)
         x1_b1_1 = self.maxpool2_b1_1(x1_b1_1)
         x1_b2_1 = self.maxpool1_b2_1(x1_b2_1)
         x1_b1_1 = self.batchnorm1_b1_1(x1_b1_1)
         x1_b2_1 = self.batchnorm1_b2_1(x1_b2_1)
 
         x2_b1_1 = self.conv2_b1_1(x1_b1_1)
+        x2_b1_1 = self.activation2_b1_1(x2_b1_1)
         x2_b1_2 = self.conv2_b1_2(x1_b1_1)
+        x2_b1_2 = self.activation2_b1_2(x2_b1_2)
         x2_b2_1 = self.conv2_b2_1(x1_b2_1)
+        x2_b2_1 = self.activation2_b2_1(x2_b2_1)
         x2_b2_2 = self.conv2_b2_2(x1_b2_1)
+        x2_b2_2 = self.activation2_b2_2(x2_b2_2)
         x2_b1_1 = self.maxpool2_b1_1(x2_b1_1)
         x2_b1_2 = self.maxpool2_b1_2(x2_b1_2)
         x2_b2_1 = self.maxpool2_b2_1(x2_b2_1)
@@ -150,21 +187,38 @@ class ColorNet(torch.nn.Module):
         x2_b2_2 = self.batchnorm2_b2_2(x2_b2_2)
 
         x3_b1_1 = self.conv3_b1_1(x2_b1_1)
+        x3_b1_1 = self.activation3_b1_1(x3_b1_1)
         x3_b1_2 = self.conv3_b1_2(x2_b1_2)
+        x3_b1_2 = self.activation3_b1_2(x3_b1_2)
         x3_b2_1 = self.conv3_b2_1(x2_b2_1)
+        x3_b2_1 = self.activation3_b2_1(x3_b2_1)
         x3_b2_2 = self.conv3_b2_2(x2_b2_2)
+        x3_b2_2 = self.activation3_b2_2(x3_b2_2)
         x3_b1 =  torch.cat((x3_b1_1,x3_b1_2),dim=1)
         x3_b2 = torch.cat((x3_b2_1,x3_b2_2),dim=1)
 
         x4_b1_1 = self.conv4_b1_1(x3_b1)
+        x4_b1_1 = self.activation4_b1_1(x4_b1_1)
         x4_b1_2 = self.conv4_b1_2(x3_b1)
+        x4_b1_2 = self.activation4_b1_2(x4_b1_2)
         x4_b2_1 = self.conv4_b2_1(x3_b2)
+        x4_b2_1 = self.activation4_b2_1(x4_b2_1)
         x4_b2_2 = self.conv4_b2_2(x3_b2)
+        x4_b2_2 = self.activation4_b2_2(x4_b2_2)
+
 
         x5_b1_1 = self.conv5_b1_1(x4_b1_1)
+        x5_b1_1 = self.activation5_b1_1(x5_b1_1)
+
         x5_b1_2 = self.conv5_b1_2(x4_b1_2)
+        x5_b1_2 = self.activation5_b1_2(x5_b1_2)
+
         x5_b2_1 = self.conv5_b2_1(x4_b2_1)
+        x5_b2_1 = self.activation5_b2_1(x5_b2_1)
+
         x5_b2_2 = self.conv5_b2_2(x4_b2_2)
+        x5_b2_2 = self.activation5_b2_2(x5_b2_2)
+
         x5_b1_1 = self.maxpool5_b1_1(x5_b1_1)
         x5_b1_2 = self.maxpool5_b1_2(x5_b1_2)
         x5_b2_1 = self.maxpool5_b2_1(x5_b2_1)
@@ -175,12 +229,15 @@ class ColorNet(torch.nn.Module):
 
         if not self.lite:
             xfc = self.fc1(xfc)
+            xfc = self.activation_fc1(xfc)
             xfc = self.dropout1(xfc)
             xfc = self.fc2(xfc)
+            xfc = self.activation_fc2(xfc)
             xfc = self.dropout2(xfc)
             xfc = self.fc3(xfc)
         else:
             xfc = self.fc1(xfc)
+            xfc = self.activation_fc1(xfc)
             xfc = self.dropout1(xfc)
             xfc = self.fc2(xfc)
         return xfc
