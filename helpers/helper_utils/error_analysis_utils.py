9b86d46f1755a63e3a04335ccc7c652f02434667
jasdeep06 <jasdeepchhabra94@gmail.com>
2022-08-04 11:13:36 +0200

    modify get url

--- helpers/helper_utils/error_analysis_utils.py
+++ helpers/helper_utils/error_analysis_utils.py
@@ -1,11 +1,11 @@
 import requests
 
 def get_url(url,port):
-    try:
-        ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4").text
-        ip = "http://" + ip  + ":" + str(port) + "/" + url
-    except:
-        ip = "http://localhost:"+ str(port) + "/" + url
+    #try:
+    #    ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4").text
+    #    ip = "http://" + ip  + ":" + str(port) + "/" + url
+    #except:
+    ip = "http://localhost:"+ str(port) + "/" + url
 
     return ip
 
